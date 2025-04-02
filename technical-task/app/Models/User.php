0ca0a2b2de53105f8d769df58e0613098bb77c4a
darko.popovski <darkopopovski39@yahoo.com>
2025-04-02 16:18:17 +0200

    modifying fields of users for task requirements

--- technical-task/app/Models/User.php
+++ technical-task/app/Models/User.php
@@ -18,9 +18,11 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'full_name',
         'email',
-        'password',
+        'phone',
+        'date_of_birth',
+        'password'
     ];
 
     /**
